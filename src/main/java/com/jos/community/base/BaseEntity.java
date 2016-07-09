399d9e91c93ea80120fb87e7ec05a9b173e51984
myttrx <593135144@qq.com>
2016-07-09 16:21:05 +0800

    Entity基础类

---
+++ src/main/java/com/jos/community/base/BaseEntity.java
@@ -0,0 +1,65 @@
+package com.jos.community.base;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@MappedSuperclass
+public abstract class BaseEntity implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -3811980688239029585L;
+
+	@Column(name = "created_by")
+	private String createdBy;
+
+	@Temporal(TemporalType.DATE)
+	@Column(name = "created_date")
+	private Date createdDate;
+
+	@Column(name = "modified_by")
+	private String modifiedBy;
+
+	@Temporal(TemporalType.DATE)
+	@Column(name = "modified_date")
+	private Date modifiedDate;
+
+	public String getCreatedBy() {
+		return createdBy;
+	}
+
+	public void setCreatedBy(String createdBy) {
+		this.createdBy = createdBy;
+	}
+
+	public Date getCreatedDate() {
+		return createdDate;
+	}
+
+	public void setCreatedDate(Date createdDate) {
+		this.createdDate = createdDate;
+	}
+
+	public String getModifiedBy() {
+		return modifiedBy;
+	}
+
+	public void setModifiedBy(String modifiedBy) {
+		this.modifiedBy = modifiedBy;
+	}
+
+	public Date getModifiedDate() {
+		return modifiedDate;
+	}
+
+	public void setModifiedDate(Date modifiedDate) {
+		this.modifiedDate = modifiedDate;
+	}
+
+}
