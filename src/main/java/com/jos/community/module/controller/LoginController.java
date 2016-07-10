c074d70e18409206a58dadb1e7470d166c2b2174
myttrx <593135144@qq.com>
2016-07-11 00:24:34 +0800

    添加登录控制器

---
+++ src/main/java/com/jos/community/module/controller/LoginController.java
@@ -0,0 +1,44 @@
+package com.jos.community.module.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.Validator;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.jos.community.module.vo.LoginUserVo;
+
+@Controller
+@RequestMapping("/")
+public class LoginController {
+
+	@Autowired
+	@Qualifier(value = "loginUserValidator")
+	private Validator loginUserValidator;
+
+	@InitBinder
+	private void initBinder(WebDataBinder binder) {
+		binder.setValidator(loginUserValidator);
+	}
+
+	@RequestMapping(value = "login", method = RequestMethod.GET)
+	public String index(Model model) {
+		return "/account/login";
+	}
+
+	@RequestMapping(value = "login", method = RequestMethod.POST)
+	public String login(@ModelAttribute @Validated LoginUserVo loginUserVo, BindingResult result) {
+		if (result.hasErrors()) {
+			return "/account/login";
+		}
+		return "main";
+	}
+	
+}
