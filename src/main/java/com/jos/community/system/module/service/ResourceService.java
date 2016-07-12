07f94272aa2f4df8b18498aeb4f419c84aa2b4e0
myttrx <593135144@qq.com>
2016-07-12 23:31:58 +0800

    使用spring security 作为系统的security框架，添加spring security配置文件和核心类

---
+++ src/main/java/com/jos/community/system/module/service/ResourceService.java
@@ -0,0 +1,10 @@
+package com.jos.community.system.module.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class ResourceService {
+	
+}
