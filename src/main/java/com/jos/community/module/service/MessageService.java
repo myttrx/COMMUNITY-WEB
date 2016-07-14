f709ade0e56bd7e18418cbe7047f6b9dd761d5f8
myttrx <593135144@qq.com>
2016-07-14 21:18:15 +0800

    继承访问国际化信息的MessageSource接口

---
+++ src/main/java/com/jos/community/module/service/MessageService.java
@@ -0,0 +1,47 @@
+package com.jos.community.module.service;
+
+import java.util.Locale;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.MessageSourceResolvable;
+import org.springframework.context.NoSuchMessageException;
+import org.springframework.stereotype.Service;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+@Service
+public class MessageService implements MessageSource{
+
+	@Autowired
+	private MessageSource messageSource;
+	
+	public String getMessage(String code, Object[] args, String defaultMessage,
+			Locale locale) {
+		return messageSource.getMessage(code, args, defaultMessage, locale);
+	}
+
+	public String getMessage(String code, Object[] args, Locale locale)
+			throws NoSuchMessageException {
+		return messageSource.getMessage(code, args, locale);
+	}
+	
+	public String getMessage(MessageSourceResolvable resolvable, Locale locale)
+			throws NoSuchMessageException {
+		return messageSource.getMessage(resolvable, locale);
+	}
+
+	public String getMessage(String code, Object[] args, String defaultMessage) {
+		HttpServletRequest request = ((ServletRequestAttributes)RequestContextHolder.getRequestAttributes()).getRequest();
+		Locale selectedLocale = (Locale)request.getSession().getAttribute(SessionLocaleResolver.LOCALE_SESSION_ATTRIBUTE_NAME);
+		if(selectedLocale==null) selectedLocale=request.getLocale();
+		return messageSource.getMessage(code, args, defaultMessage, selectedLocale);
+	}
+	
+	public String getMessage(String code, String defaultMessage) {
+		return this.getMessage(code, null, defaultMessage);
+	}
+}
