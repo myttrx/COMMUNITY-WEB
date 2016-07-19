176e5151bbe0f9d1431f18fa7651a22bb9ccdff5
myttrx <593135144@qq.com>
2016-07-19 20:59:37 +0800

    改成TemporalType.TIMESTAMP

--- src/main/java/com/jos/community/base/BaseEntity.java
+++ src/main/java/com/jos/community/base/BaseEntity.java
@@ -27,14 +27,14 @@ public abstract class BaseEntity implements Serializable {
 	@Column(name = "created_by")
 	private String createdBy;
 
-	@Temporal(TemporalType.DATE)
+	@Temporal(TemporalType.TIMESTAMP)
 	@Column(name = "created_date")
 	private Date createdDate;
 
 	@Column(name = "modified_by")
 	private String modifiedBy;
 
-	@Temporal(TemporalType.DATE)
+	@Temporal(TemporalType.TIMESTAMP)
 	@Column(name = "modified_date")
 	private Date modifiedDate;
 
