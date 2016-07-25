54b605513e04e4d5e76d1b8ca7673586b56ff45f
myttrx <593135144@qq.com>
2016-07-25 09:51:21 +0800

    添加全局静态常量类

---
+++ src/main/java/com/jos/community/utils/Constant.java
@@ -0,0 +1,10 @@
+package com.jos.community.utils;
+
+public class Constant {
+
+	public static class JqGridSord{
+		public final static String DESC = "desc";
+		public final static String ASC = "asc";
+	}
+	
+}
