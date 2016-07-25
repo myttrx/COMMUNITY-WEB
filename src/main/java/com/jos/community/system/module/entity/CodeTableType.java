e4b5b04b72dfadb13e1d86d7433f2f80a1b63947
myttrx <593135144@qq.com>
2016-07-25 09:21:57 +0800

    添加code_table entity

---
+++ src/main/java/com/jos/community/system/module/entity/CodeTableType.java
@@ -0,0 +1,12 @@
+package com.jos.community.system.module.entity;
+
+public enum CodeTableType{  
+	ROLE,//用户角色
+	RESOURCE,//资源权限
+//	PAGE,
+//	URL,
+//	URL_FILTER,
+//  MENU,
+//  COMPONENT,
+//  STRING
+}  
