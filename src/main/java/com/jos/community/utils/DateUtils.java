90fa06ed3057b0dd12ed1c5ee146efd14de20aac
myttrx <593135144@qq.com>
2016-07-26 23:43:55 +0800

    添加字符转时间，时间转字符，时间格式等方法

--- src/main/java/com/jos/community/utils/DateUtils.java
+++ src/main/java/com/jos/community/utils/DateUtils.java
@@ -1,5 +1,73 @@
 package com.jos.community.utils;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.time.DateFormatUtils;
+
 public class DateUtils extends org.apache.commons.lang3.time.DateUtils{
 
+    public static final String MONTH_FORMAT = "yyyy-MM";  
+    public static final String DATE_FORMAT = "yyyy-MM-dd";  
+    public static final String HOUR_FORMAT = "HH:mm:ss";  
+    public static final String DATETIME_FORMAT = "yyyy-MM-dd HH:mm:ss";
+    
+    /**
+     * @funName: convertDateToStr 
+     * @description: default use DATE_FORMAT
+     * @param date
+     * @return String 
+     */
+    public static String convertDateToStr(Date date) {
+		return convertDateToStr(date,DATE_FORMAT);
+	}
+	
+	public static String convertDateToStr(Date date,String pattern) {
+		String str = null;
+		try {
+			str = DateFormatUtils.format(date, pattern,Locale.ENGLISH);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+		return str;
+	}
+	
+	public static Date convertStrToDate(String str) {
+		return convertStrToDate(str,DATE_FORMAT);
+	}
+	
+	public static Date convertStrToDate(String str, String pattern) {
+		SimpleDateFormat sdf = null;
+		Date date = null;
+		try {
+			if (StringUtils.isNotBlank(pattern)) {
+				sdf = new SimpleDateFormat(pattern,Locale.ENGLISH);
+				date = sdf.parse(str);
+			} else {
+				sdf = new SimpleDateFormat(DATE_FORMAT);
+				date = sdf.parse(str);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return date;
+	}
+	
+	public static Date formatDate(Date date){
+		return formatDate(date,DATE_FORMAT);
+	}
+	
+	public static Date formatDate(Date date,String pattern){
+		SimpleDateFormat sdf = new SimpleDateFormat(pattern,Locale.ENGLISH);
+		try {
+			date =sdf.parse(sdf.format(date));
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		return date;
+	}
 }
