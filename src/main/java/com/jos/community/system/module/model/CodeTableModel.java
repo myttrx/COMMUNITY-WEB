f033dc5fe56f1456d41016ebd0223dcdb9f51bb3
myttrx <593135144@qq.com>
2016-07-28 14:17:30 +0800

    CodeTableModel类，相当于vo

---
+++ src/main/java/com/jos/community/system/module/model/CodeTableModel.java
@@ -0,0 +1,34 @@
+package com.jos.community.system.module.model;
+
+
+public class CodeTableModel {
+
+	private String code;
+	private String description;
+	private String codeTableType;
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getCodeTableType() {
+		return codeTableType;
+	}
+
+	public void setCodeTableType(String codeTableType) {
+		this.codeTableType = codeTableType;
+	}
+
+}
