502ee8cd6fd6b190c67dfca3050c00bd12ecaf09
myttrx <593135144@qq.com>
2016-08-02 22:47:49 +0800

    添加国际化代码常量

--- src/main/java/com/jos/community/utils/Constant.java
+++ src/main/java/com/jos/community/utils/Constant.java
@@ -7,4 +7,9 @@ public class Constant {
 		public final static String ASC = "asc";
 	}
 	
+	public static class MsgCode{
+		public final static String SYSTEM_ERROR = "system.error";
+		public final static String RECORD_SAVE_SUCCESS = "record.save.success";
+		public final static String RECORD_DELETE_SUCCESS = "system.delete.success";
+	}
 }
